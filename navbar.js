cff6c75eade87cedb6767844e2ce911c73e92576
Altronxs <kylextron101098@gmail.com>
2025-04-16 21:14:08 -1000

    made the navbar fixed at a certain point

--- navbar.js
+++ navbar.js
@@ -9,23 +9,43 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }         
         document.getElementById('wbl').style.height = '9vh';
+        document.getElementById('wbl').style.visibility = 'visible';
         document.getElementById('wblt').style.backgroundColor = '#BA181B';
         document.getElementById('wblt').style.color = 'white';
+        document.querySelectorAll(".wbltt").forEach(element => {
+            element.style.bottom = "0px"; // Change to "0px" if needed
+            element.style.opacity = "1";
+        });
     });
     document.getElementById('wbl').addEventListener('mouseenter', () => {
         document.getElementById('wbl').style.height = '9vh';
+        document.getElementById('wbl').style.visibility = 'visible';
         document.getElementById('wblt').style.backgroundColor = '#BA181B';
         document.getElementById('wblt').style.color = 'white';
+        document.querySelectorAll(".wbltt").forEach(element => {
+            element.style.bottom = "0px"; // Change to "0px" if needed
+            element.style.opacity = "1";
+        });
     })
     document.getElementById('wblt').addEventListener('mouseleave', () => {
         document.getElementById('wbl').style.height = '0vh';
-        document.getElementById('wblt').style.backgroundColor = '#F5F3F4'
-        document.getElementById('wblt').style.color = '#161A1D'
+        document.getElementById('wbl').style.visibility = 'hidden';
+        document.getElementById('wblt').style.backgroundColor = '#F5F3F4';
+        document.getElementById('wblt').style.color = '#161A1D';
+        document.querySelectorAll(".wbltt").forEach(element => {
+            element.style.bottom = "9vh"; // Change to "0px" if needed
+            element.style.opacity = "0";
+        });
     });
     document.getElementById('wbl').addEventListener('mouseleave', () => {
         document.getElementById('wbl').style.height = '0vh';
-        document.getElementById('wblt').style.backgroundColor = '#F5F3F4'
-        document.getElementById('wblt').style.color = '#161A1D'
+        document.getElementById('wbl').style.visibility = 'hidden';
+        document.getElementById('wblt').style.backgroundColor = '#F5F3F4';
+        document.getElementById('wblt').style.color = '#161A1D';
+        document.querySelectorAll(".wbltt").forEach(element => {
+            element.style.bottom = "9vh"; // Change to "0px" if needed
+            element.style.opacity = "0";
+        });
     })
 
     document.getElementById('ext').addEventListener('mouseenter', () => {
@@ -36,25 +56,56 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }     
         document.getElementById('extra').style.height = '9vh';
+        document.getElementById('extra').style.visibility = 'visible';
         document.getElementById('ext').style.backgroundColor = '#BA181B';
         document.getElementById('ext').style.color = 'white';
+        document.querySelectorAll(".extt").forEach(element => {
+            element.style.bottom = "0px"; // Change to "0px" if needed
+            element.style.opacity = "1";
+        });
     });
     document.getElementById('extra').addEventListener('mouseenter', () => {
         document.getElementById('extra').style.height = '9vh';
+        document.getElementById('extra').style.visibility = 'visible';
         document.getElementById('ext').style.backgroundColor = '#BA181B';
         document.getElementById('ext').style.color = 'white';
+        document.querySelectorAll(".extt").forEach(element => {
+            element.style.bottom = "0px"; // Change to "0px" if needed
+            element.style.opacity = "1";
+        });
     })
     document.getElementById('ext').addEventListener('mouseleave', () => {
         document.getElementById('extra').style.height = '0vh';
+        document.getElementById('extra').style.visibility = 'hidden';
         document.getElementById('ext').style.backgroundColor = '#F5F3F4';
         document.getElementById('ext').style.color = '#161A1D';
+        document.querySelectorAll(".extt").forEach(element => {
+            element.style.bottom = "9vh"; // Change to "0px" if needed
+            element.style.opacity = "0";
+        });
     });
     document.getElementById('extra').addEventListener('mouseleave', () => {
         document.getElementById('extra').style.height = '0vh';
+        document.getElementById('extra').style.visibility = 'hidden';
         document.getElementById('ext').style.backgroundColor = '#F5F3F4';
         document.getElementById('ext').style.color = '#161A1D';
+        document.querySelectorAll(".extt").forEach(element => {
+            element.style.bottom = "9vh"; // Change to "0px" if needed
+            element.style.opacity = "0";
+        });
     })
-
+    window.addEventListener("scroll", () => {
+        var navbar = document.getElementById('navbar');
+        if (window.location.pathname.endsWith('index.html')) {
+            const triggerPosition = window.innerHeight - (window.innerHeight * 0.08);
+            if (window.scrollY > triggerPosition) {
+                navbar.classList.add("fixed");
+            } else {
+                navbar.classList.remove("fixed");
+            }
+        }
+    });
+    
 });
 
 function redirect(url) {
