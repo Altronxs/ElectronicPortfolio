85a46c191df580bec9747309f9999b1ae9722f10
Altronxs <kylextron101098@gmail.com>
2025-05-20 13:10:47 -1000

    made name typeable effect

--- index.js
+++ index.js
@@ -3,9 +3,40 @@ window.onload = function () {
     const myroles = ["Full Stack Developer", "Immortal Lion Council Member", "Immortal Lion", "Leader", "Marauder", "Scirauder", "Photographer"];
     var random;
     var count = 0;
+    var countName = 0;
+    var nameid = 0;
+    var nameText = ["Kyle Alexander Baldovi", "AltronX"];
+    const name = document.getElementById('name');
     updateRole();
 
-    const name = document.getElementById('name');
+    function changeName()  {
+        if (nameid == 1) {
+            name.innerText = "";
+            countName = 0;
+            nameEffect();
+        } 
+        if (nameid == 0) {
+            name.innerText = "";
+            countName = 0;
+            nameEffect();
+        }
+    }
+    changeName();
+    function nameEffect() {
+        if (countName < nameText[nameid].length) {
+            name.innerText = name.innerHTML + nameText[nameid].charAt(countName);
+            countName++;
+            setTimeout(nameEffect, 100); // Corrected function reference
+        } else {
+            if (nameid == 1) {
+                nameid = 0;
+            } else if (nameid == 0) {
+                nameid = 1;
+            }
+        }
+    }
+
+
 
     function updateRole() {
         roles.innerText = "";
@@ -31,6 +62,5 @@ window.onload = function () {
     // Trigger the update every 10 seconds
     setInterval(updateRole, 10000);
 
-    // intial play
-    updateRole();
+    setInterval(changeName, 15000)
 };
