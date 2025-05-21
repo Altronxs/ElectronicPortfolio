e2bc98da491ae93d397f8e0fd47f8dc656c2e33f
Altronxs <kylextron101098@gmail.com>
2025-05-20 23:18:08 -1000

    added varients for AltronX

--- index.js
+++ index.js
@@ -5,22 +5,16 @@ window.onload = function () {
     var count = 0;
     var countName = 0;
     var nameid = 0;
-    var nameText = ["Kyle Alexander Baldovi", "AltronX"];
+    var nameText = ["Kyle Alexander Baldovi", "AltronX", "AlexTronX", "AlternateTronX"];
     const name = document.getElementById('name');
     updateRole();
 
     function changeName()  {
-        if (nameid == 1) {
-            name.innerText = "";
-            countName = 0;
-            nameEffect();
-        } 
-        if (nameid == 0) {
-            name.innerText = "";
-            countName = 0;
-            nameEffect();
-        }
+        name.innerText = "";
+        countName = 0;
+        nameEffect();
     }
+
     changeName();
     function nameEffect() {
         if (countName < nameText[nameid].length) {
@@ -29,10 +23,15 @@ window.onload = function () {
             setTimeout(nameEffect, 100); // Corrected function reference
         } else {
             if (nameid == 1) {
-                nameid = 0;
+                nameid = 2;
             } else if (nameid == 0) {
                 nameid = 1;
+            } else if (nameid == 2) {
+                nameid = 3;
+            } else if (nameid == 3) {
+                nameid = 0;
             }
+        
         }
     }
 
@@ -47,20 +46,15 @@ window.onload = function () {
 
 
     function typeEffect() {
-        if (count == 0) {
-            roles.innerText = myroles[random].charAt(0);
-            count++;
-            console.log("test");
-            setTimeout(typeEffect, 200);
-        } else {
+        if (count < myroles[random].length) {
             roles.innerText = roles.innerHTML + myroles[random].charAt(count);
             count++;
-            setTimeout(typeEffect, 200);
+            setTimeout(typeEffect, 100);
         }
     }
     
     // Trigger the update every 10 seconds
     setInterval(updateRole, 10000);
 
-    setInterval(changeName, 15000)
+    setInterval(changeName, 25000);
 };
