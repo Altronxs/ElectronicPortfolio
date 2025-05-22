abf80fb3ace57105bc455d921dc23df95229fa3d
Altronxs <kylextron101098@gmail.com>
2025-05-22 08:44:38 -1000

    added other names

--- index.js
+++ index.js
@@ -5,23 +5,17 @@ window.onload = function () {
     var count = 0;
     var countName = 0;
     var nameid = 0;
-    var nameText = ["Kyle Alexander Baldovi", "AltronX"];
+    var nameText = ["Kyle Alexander Baldovi", "AltronX", "AlexTronX", "AlternateTronX"];
     const name = document.getElementById('name');
     updateRole();
-    changeName();
 
     function changeName()  {
-        if (nameid == 1) {
-            name.innerText = "";
-            countName = 0;
-            nameEffect();
-        } 
-        if (nameid == 0) {
-            name.innerText = "";
-            countName = 0;
-            nameEffect();
-        }
+        name.innerText = "";
+        countName = 0;
+        nameEffect();
     }
+
+    changeName();
     function nameEffect() {
         if (countName < nameText[nameid].length) {
             name.innerText = name.innerHTML + nameText[nameid].charAt(countName);
@@ -29,33 +23,38 @@ window.onload = function () {
             setTimeout(nameEffect, 100); // Corrected function reference
         } else {
             if (nameid == 1) {
-                nameid = 0;
+                nameid = 2;
             } else if (nameid == 0) {
                 nameid = 1;
+            } else if (nameid == 2) {
+                nameid = 3;
+            } else if (nameid == 3) {
+                nameid = 0;
             }
+        
         }
     }
+
+
+
     function updateRole() {
         roles.innerText = "";
         count = 0;
         random = Math.floor(Math.random() * myroles.length);
         typeEffect();
     }
+
+
     function typeEffect() {
-        if (count == 0) {
-            roles.innerText = myroles[random].charAt(0);
-            count++;
-            console.log("test");
-            setTimeout(typeEffect, 200);
-        } else {
+        if (count < myroles[random].length) {
             roles.innerText = roles.innerHTML + myroles[random].charAt(count);
             count++;
-            setTimeout(typeEffect, 200);
+            setTimeout(typeEffect, 100);
         }
     }
     
     // Trigger the update every 10 seconds
     setInterval(updateRole, 10000);
 
-    setInterval(changeName, 15000)
+    setInterval(changeName, 25000);
 };
